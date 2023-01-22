300ba58c2d41af45b8d4c0f401b6cb1da1cf04c6
selfstudymaster <webselfmaster@gmail.com>
2023-01-22 14:00:46 +0900

    first commit

---
+++ CodingPractice_Cpp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodingPractice_Cpp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
